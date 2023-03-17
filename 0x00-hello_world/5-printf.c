4a08eaccde9f1a73c9626de93e72a28fc2f93e29
lekedomel <lekedomel@gmail.com>
2023-03-17 02:55:40 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is main work
+ *
+ * Return: 0 when the function as concluded
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
