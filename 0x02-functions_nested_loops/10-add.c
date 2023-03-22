b9a848f69dea1c2b18d5f69e6e150d041c5abc6b
lekedomel <lekedomel@gmail.com>
2023-03-22 15:19:56 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - sum of numbers
+ *
+ * @a: parameter 1
+ *
+ * @b: parameter 2
+ *
+ * Return: sum of values
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
