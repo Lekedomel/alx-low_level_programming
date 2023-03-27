ae07e47216072695f46b07ab6991f0e4d699954b
lekedomel <lekedomel@gmail.com>
2023-03-27 09:38:56 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,18 +2,18 @@
 
 /**
  * print_rev - print in reverse
- * @s: string of value
+ * @str: string of value
  * Return: void
  */
 
-void print_rev(char *s)
+void print_rev(char *str)
 {
 	int i;
 	int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	for (i = count; i >= 0; i--)
-		_putchar(s[i]);
+		_putchar(str[i]);
 	_putchar('\n');
 }
