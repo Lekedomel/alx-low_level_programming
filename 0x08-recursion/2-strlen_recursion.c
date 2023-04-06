2efb00f69e2119472cb2ef5b55669c7c750fad1e
lekedomel <lekedomel@gmail.com>
2023-04-06 06:29:02 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of string
+ * @s: pointer
+ * Return: print length
+ */
+
+
+
+
+
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
+
